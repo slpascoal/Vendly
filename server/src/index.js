a3227cc98c075afbd54384ddcb3fe4fce6cb30c8
Silas L. Pascoal <slpascoal01@gmail.com>
2024-10-11 13:13:41 -0300

    Criação do Mongo e ENV

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,15 @@
 import express from "express"
 import cors from 'cors'
+import { Mongo } from "./database/mongo"
+import { config } from "dotenv";
 
 async function server() {
     const hostName = 'localhost'
     const port = 3000
 
     const app = express()
+
+    const mongoConnection = await Mongo.connect({mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME})
 
     app.use(express.json())
     app.use(cors())
