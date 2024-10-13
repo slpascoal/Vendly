1a9b4f69de5db3b83f89c78785236a00177c3822
Silas L. Pascoal <slpascoal01@gmail.com>
2024-10-13 17:38:17 -0300

    Conexão ao Banco de Dados e Biome.json

--- server/src/database/mongo.js
+++ server/src/database/mongo.js
@@ -1,20 +1,19 @@
-import { MongoClient } from "mongodb";
+import { MongoClient } from 'mongodb'
 
 export const Mongo = {
-    async connect({ mongoConnectionString, mongoDbName }) {
-        try {
-            const client = new MongoClient(mongoConnectionString)
+  async connect({ mongoConnectionUrl, mongoDbName }) {
+    try {
+      const client = new MongoClient(mongoConnectionUrl)
 
-            await client.connect()
-            const db = client.db(mongoDbName)
+      await client.connect()
+      const db = client.db(mongoDbName)
 
-            this.client = client
-            this.db = db
+      this.client = client
+      this.db = db
 
-            return 'Connected to mongo'
-
-        } catch (error) {
-           return { text: 'Error during mongo connection', error}
-        }
+      return 'Connected to mongo'
+    } catch (error) {
+      return { text: 'Error during mongo connection', error }
     }
+  },
 }
--- server/src/index.js
+++ server/src/index.js
@@ -1,30 +1,35 @@
-import express from "express"
+import express from 'express'
 import cors from 'cors'
-import { Mongo } from "./database/mongo"
-import { config } from "dotenv";
+import { Mongo } from './database/mongo.js'
+import { config } from 'dotenv'
+
+config()
 
 async function server() {
-    const hostName = 'localhost'
-    const port = 3000
+  const hostName = 'localhost'
+  const port = 3000
 
-    const app = express()
+  const app = express()
 
-    const mongoConnection = await Mongo.connect({mongoConnectionString: process.env.MONGO_CS, mongoDbName: process.env.MONGO_DB_NAME})
+  const mongoConnection = await Mongo.connect({
+    mongoConnectionUrl: process.env.MONGO_PS,
+    mongoDbName: process.env.MONGO_DB_NAME,
+  })
 
-    app.use(express.json())
-    app.use(cors())
-    
-    app.get('/', (req, res) => {
-        res.send({
-            success: true,
-            statusCode: 200,
-            body: 'Welcome to Vendly'
-        })
+  app.use(express.json())
+  app.use(cors())
+
+  app.get('/', (req, res) => {
+    res.send({
+      success: true,
+      statusCode: 200,
+      body: 'Welcome to Vendly',
     })
-    
-    app.listen(port, () => {
-        console.log(`Server running on: http://${hostName}:${port}`)
-    })
+  })
+
+  app.listen(port, () => {
+    console.log(`Server running on: http://${hostName}:${port}`)
+  })
 }
 
 server()
